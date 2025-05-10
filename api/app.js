770f522bdf2428c5b0cf16263180c1f84fdecdc6
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-10 10:28:40 +0300

    corrected app js

--- api/app.js
+++ api/app.js
@@ -1,2 +1,11 @@
-const app = require('../app');
-require(app.join(__dirname, '..app.js'));
+// const path = require('path');
+// require(path.join(__dirname, '../app.js'));
+// // console.log('hhtyhyy',__dirname)
+const express = require('express');
+const path = require('path');
+const app = require(path.join(__dirname, '../app.js'));
+
+// Wrap express app for Vercel
+module.exports = (req, res) => {
+  app(req, res);
+};
