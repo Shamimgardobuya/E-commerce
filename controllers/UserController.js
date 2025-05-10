3764761a660449534c75e2729b57570e1a36c9de
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-10 10:59:55 +0300

    fixing vercel deployment errors with bcrypt

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const model = require('../models');
 const User = model.User;
 const jwt = require('jsonwebtoken');
