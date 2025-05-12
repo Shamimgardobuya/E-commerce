68b5b80f949aa0a4beb9a66f40c94f64d8a804a0
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-12 07:48:55 +0300

    added token

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var indexRouter = express.Router();
+var cookieParser = require('cookie-parser');
+var app = express();
+const csrf = require('csurf');
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true })); // for form parsing
+const csrfProtection = csrf({ cookie: true });
 
 /* GET home page. */
 indexRouter.get('/index', (req, res) => {
   console.log('index page ');
-  res.render('index', { title: 'Expressuiu' });
+  res.render('index', { csrfToken : req.csrfToken() });
 });
 
 
