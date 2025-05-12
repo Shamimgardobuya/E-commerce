4cb5032100eb427a2c01004ff74b1e8773edb40b
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-12 10:59:32 +0300

    removed some properties from mpesa main class

--- routes/mpesa_.js
+++ routes/mpesa_.js
@@ -4,60 +4,65 @@ const moment = require("moment");
 const base64 = require('base-64');
 
 class Mpesa{
-    constructor() {
-        this.consumer_key = process.env.CONSUMER_KEY;
-        this.consumer_secret = process.env.CONSUMER_SECRET;
-        this.callback_url = process.env.MPESA_CALLBACK_URL;
-        this.validation_url = process.env.MPESA_VALIDATION_URL;
-        this.shortcode = process.env.MPESA_SHORTCODE;
-        this.mpesa_base_url = process.env.MPESA_BASE_URL;
-        this.passkey = process.env.PASS_KEY
-        this.accountRef = process.env.AccountReference
+    constructor () {
+        this.token = null;
+
     }
     async generateToken() {
         const timestamp = moment().unix();
         const req = unirest("GET", "https://sandbox.safaricom.co.ke/oauth/v1/generate");
-        let token;
         req.query({
             "grant_type": "client_credentials"
         });
         req.headers({
-            "Authorization": `Basic ${this.consumer_key}:${this.consumer_secret}`
+            "Authorization": `Basic ${process.env.CONSUMER_KEY}:${process.env.CONSUMER_SECRET}`
         });
         req.end(res => {
             if (res.error) throw new Error(res.error);
             console.log(res.body);
-            token = res.body.access_token;
+            this.token = res.body.access_token;
         });
-        return token;
+        console.log('tokenn', this.token)
+        return this.token;
     }
-    async registerCallback(token) {
-        unirest.post(`${this.mpesa_base_url}/mpesa/callback/register`)
-            .headers({ 'Authorization': `Bearer  ${token}`, 'Content-Type': 'application/json' })
-            .send({
-                confirmation_url: this.callback_url,
-                validation_url: this.validation_url,
-                consumer_key: this.consumer_key,
-                consumer_secret: this.consumer_secret,
-                shortcode: this.shortcode
-            })
-            .then((response) => {
-                console.log(response.body)
-            })
+    async registerCallback() {
+        if (this.token) {
+
+            let mpesa_base_url = process.env.MPESA_BASE_URL;
+            unirest.post(`${mpesa_base_url}/mpesa/callback/register`)
+                .headers({ 'Authorization': `Bearer  ${this.token}`, 'Content-Type': 'application/json' })
+                .send({
+                    confirmation_url: process.env.MPESA_CALLBACK_URL,
+                    validation_url: process.env.MPESA_VALIDATION_URL,
+                    consumer_key: process.env.CONSUMER_KEY,
+                    consumer_secret: process.env.CONSUMER_SECRET,
+                    shortcode: process.env.MPESA_SHORTCODE
+                })
+                .then((response) => {
+                    console.log(response.body)
+                })
+            }
     }
-    async processRequest(token, amount, phoneNumber) {
+    async processRequest( amount, phoneNumber) {
         const timestamp = moment().unix();
-        unirest.post(`${this.mpesa_base_url}/mpesa/stkpush/v1/processrequest`)
+        let passkey = process.env.PASS_KEY;
+        let mpesa_base_url = process.env.MPESA_BASE_URL;
+
+
+        if (this.token) {
+
+        
+        unirest.post(`${mpesa_base_url}/mpesa/stkpush/v1/processrequest`)
             .headers({ 'Authorization': `Bearer  ${token}`, 'Content-Type': 'application/json' })
             .send({
-                "BusinessShortCode": this.shortcode,
-                "Password": base64.encode(this.shortcode + this.passkey + timestamp),
+                "BusinessShortCode": process.env.MPESA_SHORTCODE,
+                "Password": base64.encode(process.env.MPESA_SHORTCODE + passkey + timestamp),
                 "Timestamp": timestamp,
                 "TransactionType": "CustomerPayBillOnline",
                 "Amount": amount,
                 "PartyA": phoneNumber,
                 "PartyB": this.shortcode,
-                "PhoneNumber": "254708374149",
+                "PhoneNumber": phoneNumber,
                 "CallBackURL": this.callback_url,
                 "AccountReference": this.accountRef,
                 "TransactionDesc": "Test"
@@ -65,6 +70,7 @@ class Mpesa{
             .then((response) => {
                 console.log(response.body)
             })
+        }
     }
 }
 module.exports = {
