3a4d34a5c6762d27d33e1940770df468b8d016f3
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-12 11:41:58 +0300

    replaced unirest with axios

--- routes/mpesa_.js
+++ routes/mpesa_.js
@@ -2,75 +2,91 @@ require('dotenv').config()
 var unirest = require('unirest');
 const moment = require("moment");
 const base64 = require('base-64');
+const axios = require('axios');
+const { response } = require('../app');
 
 class Mpesa{
     constructor () {
         this.token = null;
+        this.base64 = Buffer.from(`${process.env.CONSUMER_KEY}:${process.env.CONSUMER_SECRET}`).toString('base64');
+
 
     }
-    generateToken() {
-        const timestamp = moment().unix();
-        const req = unirest("GET", "https://sandbox.safaricom.co.ke/oauth/v1/generate");
-        req.query({
-            "grant_type": "client_credentials"
-        });
-        req.headers({
-            "Authorization": `Basic ${process.env.CONSUMER_KEY}:${process.env.CONSUMER_SECRET}`
-        });
-        req.end(res => {
-            if (res.error) throw new Error(res.error);
-            console.log(res.body);
-            this.token = res.body.access_token;
-        });
-        console.log('tokenn', this.token)
-        return this.token;
+    async generateToken() {
+        try {
+            const response = await axios.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
+                headers: {
+                    'Authorization': `Basic ${this.base64}`
+                }
+            });
+            this.token = response.data.access_token;
+            console.log('Token:', this.token);
+            return this.token;
+        } catch (error) {
+            console.error('Token error:', error);
+            throw error;
+        }
     }
-    registerCallback() {
-        if (this.token) {
-
+    async registerCallback() {
+        try {
             let mpesa_base_url = process.env.MPESA_BASE_URL;
-            unirest.post(`${mpesa_base_url}/mpesa/callback/register`)
-                .headers({ 'Authorization': `Bearer  ${this.token}`, 'Content-Type': 'application/json' })
-                .send({
-                    confirmation_url: process.env.MPESA_CALLBACK_URL,
-                    validation_url: process.env.MPESA_VALIDATION_URL,
-                    consumer_key: process.env.CONSUMER_KEY,
-                    consumer_secret: process.env.CONSUMER_SECRET,
-                    shortcode: process.env.MPESA_SHORTCODE
-                })
-                .then((response) => {
-                    console.log(response.body)
-                })
-            }
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.token}`,
+            };
+            const data = {
+                confirmation_url: process.env.MPESA_CALLBACK_URL,
+                validation_url: process.env.MPESA_VALIDATION_URL,
+                consumer_key: process.env.CONSUMER_KEY,
+                consumer_secret: process.env.CONSUMER_SECRET,
+                shortcode: process.env.MPESA_SHORTCODE
+            } 
+            await axios.post(`${mpesa_base_url}/mpesa/callback/register`, data,
+                {headers})
+            
+            console.log('Response data:', response.data);
+            return response.data;
+
+        } catch (error) {
+            console.error('Request failed:', error.response ? error.response.data : error.message);
+            return 'Request failed:', error.response ? error.response.data : error.message
+            
+        }
+
     }
-    processRequest( amount, phoneNumber) {
+    async processRequest( amount, phoneNumber) {
         const timestamp = moment().unix();
         let passkey = process.env.PASS_KEY;
         let mpesa_base_url = process.env.MPESA_BASE_URL;
-
-
-        if (this.token) {
-
-        
-        unirest.post(`${mpesa_base_url}/mpesa/stkpush/v1/processrequest`)
-            .headers({ 'Authorization': `Bearer  ${token}`, 'Content-Type': 'application/json' })
-            .send({
-                "BusinessShortCode": process.env.MPESA_SHORTCODE,
-                "Password": base64.encode(process.env.MPESA_SHORTCODE + passkey + timestamp),
-                "Timestamp": timestamp,
-                "TransactionType": "CustomerPayBillOnline",
-                "Amount": amount,
-                "PartyA": phoneNumber,
-                "PartyB": this.shortcode,
-                "PhoneNumber": phoneNumber,
-                "CallBackURL": this.callback_url,
-                "AccountReference": this.accountRef,
-                "TransactionDesc": "Test"
-            })
-            .then((response) => {
-                console.log(response.body)
-            })
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token}`,
+        };
+        const data = {
+            "BusinessShortCode": process.env.MPESA_SHORTCODE,
+            "Password": base64.encode(process.env.MPESA_SHORTCODE + passkey + timestamp),
+            "Timestamp": timestamp,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phoneNumber,
+            "PartyB": this.shortcode,
+            "PhoneNumber": phoneNumber,
+            "CallBackURL": this.callback_url,
+            "AccountReference": this.accountRef,
+            "TransactionDesc": "Test"
         }
+        try {
+            await axios.post(`${mpesa_base_url}/mpesa/stkpush/v1/processrequest`, data, headers)
+            console.log('Response data:', response.data);
+            return response.data;
+
+
+        } catch (error) {
+            console.error('Request failed:', error.response ? error.response.data : error.message);
+            return 'Request failed:', error.response ? error.response.data : error.message;   
+
+        }
+        
     }
 }
 module.exports = {
