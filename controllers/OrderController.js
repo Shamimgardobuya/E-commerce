3865353ef065fc9d07af59aadf685569f9d3f9b7
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-14 18:49:16 +0300

    connected redis with env credentials

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -6,9 +6,15 @@ const Product = model.Product;
 const { createClient } = require('redis');
 
 const client = createClient(
-
-  
-
+{
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
+    retryStrategy: (times) => {
+        const delay = Math.min(times * 50, 2000);
+        return delay;
+}
+}
 );
 
 client.on('error', err => console.log('Redis Client Error', err));
