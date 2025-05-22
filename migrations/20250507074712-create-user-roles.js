b8fdd1bca73ce8fec4c6d2b923ed32fce54269d7
Shamimgardobuya <shamimobuya@gmail.com>
2025-05-22 17:39:44 +0300

    restored file

---
+++ migrations/20250507074712-create-user-roles.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { all } = require('../routes');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('UserRoles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model: 'Users',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      roleId: {
+        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
+        references : {
+          model: 'Roles',
+          key: 'id'
+        },
+        allowNull: false
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('UserRoles');
+  }
+};
